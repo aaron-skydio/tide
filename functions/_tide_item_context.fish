39062b716b01b1a1b86aaf31d5ab8390b8217954
Aaron Miller <aaron@skydio.com>
2023-02-01 22:26:29 -0800

    fix ssh client in vscode

--- functions/_tide_item_context.fish
+++ functions/_tide_item_context.fish
@@ -1,5 +1,5 @@
 function _tide_item_context
-    if set -q SSH_TTY
+    if set -q SSH_CLIENT
         set -fx tide_context_color $tide_context_color_ssh
     else if test "$EUID" = 0
         set -fx tide_context_color $tide_context_color_root
